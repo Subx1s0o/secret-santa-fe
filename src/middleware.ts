739acf131f41c512f07ba335b12ec70b45ebe29e
Subx1s0o <nazarsabardin@gmail.com>
2024-11-24 23:30:27 +0200

    fix: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,6 +19,12 @@ export default function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/rooms', request.url))
   }
 
+
+  if (request.nextUrl.pathname.startsWith('/rooms') && !isLogged) {
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+  }
+
+
   return NextResponse.next()
 }
 
