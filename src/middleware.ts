5f428955f3583855ba51de82705a22f72c7e912f
Subx1s0o <nazarsabardin@gmail.com>
2024-11-27 18:24:27 +0200

    fix: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,40 +1,47 @@
 
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server'
 
-const isLogged = true
 
 export default function middleware(request: NextRequest) {
   const url = new URL(request.url);
   const token = url.searchParams.get('join');
+  const session = cookies().get("session")
+
 
   if (token) {
     return NextResponse.redirect(new URL('/api/set-cookie?join=' + token, url));
   }
+
+  if (request.nextUrl.pathname === '/rules' && !session) {
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+  }
+
+  if (request.nextUrl.pathname.startsWith('/rooms') && !session) {
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+  }
+
   if (
     (request.nextUrl.pathname === '/sign-in' ||
       request.nextUrl.pathname === '/sign-up') &&
-    isLogged
+    session
   ) {
     return NextResponse.redirect(new URL('/rooms', request.url))
   }
 
-  if (request.nextUrl.pathname === '/' && !isLogged) {
+  if (request.nextUrl.pathname === '/' && !session) {
     return NextResponse.redirect(new URL('/sign-in', request.url))
   }
 
-  if (request.nextUrl.pathname === '/' && isLogged) {
+  if (request.nextUrl.pathname === '/' && session) {
     return NextResponse.redirect(new URL('/rooms', request.url))
   }
 
-
-  if (request.nextUrl.pathname.startsWith('/rooms') && !isLogged) {
-    return NextResponse.redirect(new URL('/sign-in', request.url))
-  }
 
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/', '/sign-in', '/sign-up']
+  matcher: ['/', '/sign-in', '/sign-up', "/rooms", "/rooms/:path*", '/rules']
 }
